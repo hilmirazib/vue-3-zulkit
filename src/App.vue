8ebcad1d27a80859160a3d16e912d198b63a9249
hilmi razib yusuf <hilmi.inolabs@gmail.com>
2022-12-18 13:32:07 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import Navbar from "@/Components/Navbar.vue"
+import { RouterLink, RouterView } from 'vue-router';
+import Navbar from './Components/Navbar.vue'
 </script>
 
 <template>
